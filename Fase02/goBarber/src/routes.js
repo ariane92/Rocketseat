3f1f998a17c0ce8c9db81bbca8ac4c9a4a08e0b6
Ariane Mateus <ariane.ccomp@gmail.com>
2020-03-06 07:44:09 -0300

    configurações debug

--- Fase02/goBarber/src/routes.js
+++ Fase02/goBarber/src/routes.js
@@ -3,7 +3,7 @@ import  { Router } from 'express';
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-    return res.json({teste: "Olá"})
+    return res.json({teste: "Olá Ariane"})
 })
 
 export default  routes;

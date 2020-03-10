3b7385b0a23ad41e1b7fae964dd1c11f6c7ab3b9
Ariane Mateus <ariane.ccomp@gmail.com>
2020-03-10 08:56:02 -0300

    Middleware de autenticação

--- Fase02/goBarber/src/app/controllers/UserController.js
+++ Fase02/goBarber/src/app/controllers/UserController.js
@@ -16,6 +16,11 @@ class UserController {
       provider,
     });
   }
+
+  async update(req, res) {
+    console.log(req.userId);
+    return res.status(401).json({ ok: true });
+  }
 }
 
 export default new UserController();
--- Fase02/goBarber/src/routes.js
+++ Fase02/goBarber/src/routes.js
@@ -3,9 +3,14 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMidlleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMidlleware);
+routes.put('/users', UserController.update);
 
 export default routes;

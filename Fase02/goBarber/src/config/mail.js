344df39fc8b23fbe54395973469701e7356fca6f
Ariane Mateus <ariane.ccomp@gmail.com>
2020-03-20 13:32:35 -0300

    configuraçãoes de envio de email (nodemailer)

---
+++ Fase02/goBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "1b6dfd1fe02873",
+    pass: "6261d11436ba9d"
+    },
+    default:{
+      from: 'Equipe GoBabarber <noreply@gobarber.com>'
+    }
+}

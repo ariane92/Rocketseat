24607c0c2e1337a1c87aeba1c27c9ca214d89857
Ariane Mateus <ariane.ccomp@gmail.com>
2020-03-21 10:54:12 -0300

    Listagem de horários disponiveis

--- Fase02/goBarber/src/app/controllers/AvailableController.js
+++ Fase02/goBarber/src/app/controllers/AvailableController.js
@@ -1,4 +1,12 @@
-import { startOfDay, endOfDay } from 'date-fns';
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 
@@ -15,18 +23,45 @@ class AvailableController {
     const appointments = await Appointment.findAll({
       where: {
         provider_id: req.params.providerId,
-        canceled_alt: null,
+        canceled_at: null,
         date: {
           [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
         },
       },
     });
 
-    if (!appointments) {
-      return res.json('não encontrado');
-    }
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
 
-    return res.json(appointments);
+    const avaiable = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:sssxxx"),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') == time),
+      };
+    });
+
+    return res.json(avaiable);
   }
 }
 

4d22f3adf063c04a2d46074a504741117212a4ce
Ariane Mateus <ariane.ccomp@gmail.com>
2020-03-26 17:17:33 -0300

    Estilos globais

--- frontend/src/pages/Main/index.js
+++ frontend/src/pages/Main/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Title }from './styles';
 
 function Main(){
-  return <Title error={false}>Main <small>Menor</small></Title>
+  return <Title>Main</Title>
 }
 
 export default Main;

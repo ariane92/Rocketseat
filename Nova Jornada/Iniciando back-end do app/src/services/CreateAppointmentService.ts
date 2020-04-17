5420d36ab010999cf30278e216244986a4175b1a
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-17 10:16:52 -0300

    registry creation

--- Nova Jornada/Iniciando back-end do app/src/services/CreateAppointmentService.ts
+++ Nova Jornada/Iniciando back-end do app/src/services/CreateAppointmentService.ts
@@ -4,11 +4,14 @@ import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepositoty';
 
 interface RequestDTO {
-  provider: string;
+  provider_id: string;
   date: Date;
 }
 class CreateAppointmentService {
-  public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+  public async execute({
+    provider_id,
+    date,
+  }: RequestDTO): Promise<Appointment> {
     const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
 
@@ -21,7 +24,7 @@ class CreateAppointmentService {
     }
 
     const appointment = appointmentsRepository.create({
-      provider,
+      provider_id,
       date: appointmentDate,
     });
 

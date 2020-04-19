d0cd0aac630a85660a752b44ec90ab5d5079c00d
Ariane Mateus <ariane.ccomp@gmail.com>
2020-04-18 21:41:49 -0300

    updating avatar

--- Nova Jornada/Iniciando back-end do app/src/routes/users.routes.ts
+++ Nova Jornada/Iniciando back-end do app/src/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import uploadConfig from '../config/upload';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
 const upload = multer(uploadConfig);
@@ -32,8 +33,18 @@ usersRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    console.log(request.file);
-    return response.json({ ok: true });
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avartarFileName: request.file.filename,
+      });
+      delete user.password;
+      return response.json(user);
+    } catch (err) {
+      return response.status(400).json({ error: err.message });
+    }
   },
 );
 export default usersRouter;
